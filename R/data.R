8e5ba189828d538ddd6a8684618dcb71f5d3d0a7
Mohsen <monabiyan@gmail.com>
2019-03-24 13:14:51 -0400

    passing all genes information

--- R/data.R
+++ R/data.R
@@ -5,6 +5,50 @@
 #' @format List
 #'   ...
 #' }
-#' @source \url{AMARETTO}
+#' @source \url{CommunityAMARETTO}
 "MsigdbMapping"
 
+
+#' AMARETTOinit1_tutorial
+#'
+#' An AMARETTO_init object, outputted from an AMARETTO run, on a small sample of TCGA-LIHC data 
+#'
+#' @format List
+#'   ...
+#' }
+#' @source \url{CommunityAMARETTO}
+"AMARETTOinit1_tutorial"
+
+
+#' AMARETTOinit2_tutorial
+#'
+#' An AMARETTO_init object, outputted from an AMARETTO run, on a small sample of CCLE-Liver cell lines data 
+#'
+#' @format List
+#'   ...
+#' }
+#' @source \url{CommunityAMARETTO}
+"AMARETTOinit2_tutorial"
+
+
+#' AMARETTOresults1_tutorial
+#'
+#' An AMARETTO_results object, outputted from an AMARETTO run, on a small sample of TCGA-LIHC data 
+#'
+#' @format List
+#'   ...
+#' }
+#' @source \url{CommunityAMARETTO}
+#' 
+"AMARETTOresults1_tutorial"
+
+
+#' AMARETTOresults2_tutorial
+#'
+#' An AMARETTO_results object, outputted from an AMARETTO run, on a small sample of CCLE-Liver cell lines data 
+#'
+#' @format List
+#'   ...
+#' }
+#' @source \url{CommunityAMARETTO}
+"AMARETTOresults2_tutorial"
