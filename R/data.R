3edc22b27cc713828a0a16714857f75fe74d738d
Mohsen <monabiyan@gmail.com>
2019-04-17 14:50:48 -0400

    Add sample data to package, update data.R

--- R/data.R
+++ R/data.R
@@ -8,3 +8,74 @@
 #' @source \url{CommunityAMARETTO}
 "MsigdbMapping"
 
+#' AMARETTOinit_all
+#'
+#' A dataframe containing of MSIGDB geneset names, their urls and descriptions.
+#'
+#' @format List
+#'   ...
+#' }
+#' @source \url{CommunityAMARETTO}
+"AMARETTOinit_all"
+
+#' AMARETTOresults_all
+#'
+#' A dataframe containing of MSIGDB geneset names, their urls and descriptions.
+#'
+#' @format List
+#'   ...
+#' }
+#' @source \url{CommunityAMARETTO}
+"AMARETTOresults_all"
+
+#' CNV_matrix_LIHC
+#'
+#' A dataframe containing of MSIGDB geneset names, their urls and descriptions.
+#'
+#' @format List
+#'   ...
+#' }
+#' @source \url{CommunityAMARETTO}
+"CNV_matrix_LIHC"
+
+#' Driver_List
+#'
+#' A dataframe containing of MSIGDB geneset names, their urls and descriptions.
+#'
+#' @format List
+#'   ...
+#' }
+#' @source \url{CommunityAMARETTO}
+"Driver_List"
+
+#' MA_matrix_CCLE
+#'
+#' A dataframe containing of MSIGDB geneset names, their urls and descriptions.
+#'
+#' @format List
+#'   ...
+#' }
+#' @source \url{CommunityAMARETTO}
+"MA_matrix_CCLE"
+
+
+#' MA_matrix_LIHC
+#'
+#' A dataframe containing of MSIGDB geneset names, their urls and descriptions.
+#'
+#' @format List
+#'   ...
+#' }
+#' @source \url{CommunityAMARETTO}
+"MA_matrix_LIHC"
+
+#' MET_matrix_LIHC
+#'
+#' A dataframe containing of MSIGDB geneset names, their urls and descriptions.
+#'
+#' @format List
+#'   ...
+#' }
+#' @source \url{CommunityAMARETTO}
+"MET_matrix_LIHC"
+
